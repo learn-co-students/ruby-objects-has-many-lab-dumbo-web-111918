cf6192b1ee455e365f9e5e124df044fb52868e65
github username <github email address>
2018-11-28 12:18:17 -0500

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+class Author
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def posts
+    Post.all
+  end
+
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.title = title
+    post.author = self
+  end
+
+  def self.post_count
+    Post.all.count
+  end
+
+end
